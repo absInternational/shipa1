ec0e1b32b1ee2c7e2c75c7be6d1137e4e6c31467
ABS Frontend <michaelroach397@gmail.com>
2024-04-27 00:39:21 +0500

    s

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -48,14 +48,14 @@
                     </div>
                 </div>
                 <div class="txt-loading">
-                    <span data-text-preloader="L" class="letters-loading"> L </span>
-                    <span data-text-preloader="O" class="letters-loading"> O </span>
-                    <span data-text-preloader="G" class="letters-loading"> G </span>
+                    <span data-text-preloader="S" class="letters-loading"> S </span>
+                    <span data-text-preloader="H" class="letters-loading"> H </span>
                     <span data-text-preloader="I" class="letters-loading"> I </span>
-                    <span data-text-preloader="L" class="letters-loading"> L </span>
+                    <span data-text-preloader="P" class="letters-loading"> P </span>
+                    <span data-text-preloader="" class="letters-loading">  </span>
                     <span data-text-preloader="A" class="letters-loading"> A </span>
-                    <span data-text-preloader="N" class="letters-loading"> N </span>
-                    <span data-text-preloader="D" class="letters-loading"> D </span>
+                    <span data-text-preloader="1" class="letters-loading"> 1 </span>
+                    <span data-text-preloader="" class="letters-loading">  </span>
                 </div>
             </div>
             <button class="tj-primary-btn">Cancel Preloader</button>
