0f851c78d3664813ba556bbc6122a01c101d00b0
acessonly11 <acessonly11@gmail.com>
2024-05-20 12:18:22 -0700

    f

--- resources/views/frontend/pages/privacyPolicy.blade.php
+++ resources/views/frontend/pages/privacyPolicy.blade.php
@@ -14,7 +14,7 @@
                                     <span>Home</span>
                                 </a>
                             </span>
-                            >
+                            
                             <span>
                                 <span> Privacy Policy</span>
                             </span>
@@ -25,4 +25,207 @@
         </div>
     </section>
     <!--========== breadcrumb End ==============-->
+
+     <!--========== Privacy Policy Start ==============-->
+    <section class="tj-service-details">
+        <div class="container">
+        <div class="row">
+                <div class="col-lg-12">
+                    <div class="tj-section-heading text-center">
+                        <span class="sub-title active-shape">Ship A1 Transport</span>
+                        <!-- <h2 class="title">Welcome to the  &amp; privacy policy </h2> -->
+                    </div>
+                </div>
+            </div>
+<div class="row">
+      <div class="col-lg-8">
+            <div class="page-details-wrapper service-details-wrapper">
+                        
+                        
+                 <div class="details-video-content">
+                      <h4 class="title">Welcome to the privacy policy</h4>
+                            <p>
+                            The information that Ship A1 Transport ("we," "us," "our") gathers, uses, and discloses about you when you use our services and website, www.shipa1.com (the "Site"), is outlined in this Privacy Policy. By accessing or using the Site or its services, you agree to the Privacy Policy.
+
+                            </p> 
+                            <p>
+                            By accepting and agreeing to the privacy and policy of Ship A1 Transport you therefore agree and acknowledge to be bound by the advised terms. If you deny or disagree with any of the below factors, then you won't be allowed access to our website or any of our services.
+                            </p> 
+                           
+                                
+                     
+                </div>
+                <div class="details-video-content">
+                      <h4 class="title">Information we collect</h4>
+                            <p>
+                            At Ship A1 Transport we prioritize the privacy and security of our clients and views. As per the privacy policy, we only collect information to arrange your transportation and proceed with the process according to your needs. This policy covers both personal and non-personal, browsing behavior and cookies. We ensure the awareness and accuracy of our legal obligation to safeguard our client and viewer’s information with the highest discretion and care and to use it only for the delivery of transportation services.
+
+                            </p> 
+                            <p>
+                            By accessing or using our services, users agree to our Privacy Policy and the terms outlined below.
+
+                            </p> 
+                           
+                                
+                     
+                </div>
+                <div class="details-video-content">
+                      <h4 class="title">Personal Information: </h4>
+                            <p>
+                            To book a transport Ship A1 Transport diligently collects personal information relevant to your transportation needs. 
+
+                            </p> 
+                            <p>
+                            This could include:
+
+                            </p> 
+                     
+                <div class="col-lg-6" data-sal="slide-left" data-sal-duration="800">
+                    <div class="tj-faq-left-content">
+                        
+                        <div class="faq-content">
+                            <div class="faq-icon">
+                                <i class="fa-regular fa-check pp-list"></i>
+                            </div>
+                            <div class="faq-text">
+                                <!-- <h6 class="title">Reliable & Trustworthy</h6> -->                             
+                                <p class="mb-2">Name</p>
+                            </div>
+                        </div>
+                        <div class="faq-content">
+                            <div class="faq-icon">
+                            
+
+                                <i class="fa-regular fa-check pp-list"></i>
+                            </div>
+                            <div class="faq-text">
+                                <!-- <h6 class="title">High Quality Material</h6> -->
+                                <p class="mb-2">Phone number</p>
+                            </div>
+                        </div>
+                        <div class="faq-content">
+                            <div class="faq-icon">
+                                <i class="fa-regular fa-check pp-list"></i>
+                            </div>
+                            <div class="faq-text">
+                                <!-- <h6 class="title">High Quality Material</h6> -->
+                                <p class="mb-2">Payment information </p>
+                            </div>
+                        </div>
+                        <div class="faq-content">
+                            <div class="faq-icon">
+                                <i class="fa-regular fa-check pp-list"></i>
+                            </div>
+                            <div class="faq-text">
+                                <!-- <h6 class="title">High Quality Material</h6> -->
+                                <p class="mb-2">Car details</p>
+                            </div>
+                        </div>
+                        <div class="faq-content">
+                            <div class="faq-icon">
+                                <i class="fa-regular fa-check pp-list"></i>
+                            </div>
+                            <div class="faq-text">
+                                <!-- <h6 class="title">High Quality Material</h6> -->
+                                <p class="mb-2">Pick-up city, state & zip code</p>
+                            </div>
+                        </div>
+                        <div class="faq-content">
+                            <div class="faq-icon">
+                                <i class="fa-regular fa-check pp-list"></i>
+                            </div>
+                            <div class="faq-text">
+                                <!-- <h6 class="title mb-2">High Quality Material</h6> -->
+                                <p class="mb-2">Delivery city, state & zip code</p>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+              
+                <p>
+                This information is essential to contact you about your transportation scheduling and booking
+
+
+                            </p> 
+                </div>
+                 
+                
+            </div>
+    </div>
+    
+    <div class="col-lg-4">
+                    <div class="details-sidebar-inner">
+                        <div class="tj-sidebar-widget sidebar-search">
+                            <form action="#">
+                                <input type="text" class="form-control" name="search" id="search" placeholder="Search">
+                                <i class="flaticon-loupe"></i>
+                            </form>
+                        </div>
+                        <div class="tj-sidebar-widget sidebar-service">
+                            <h5 class="details_title">Logistics Services</h5>
+                            <ul class="list-gap">
+                                                                    <li>
+                                        <a href="http://127.0.0.1:8000/services/heavy-equipment-shipping-services">Heavy Equipment Shipping Services
+                                            <i class="flaticon-right-chevron"></i>
+                                        </a>
+                                    </li>
+                                                                    <li>
+                                        <a href="http://127.0.0.1:8000/services/construction-equipment-transport">Construction Equipment Transport
+                                            <i class="flaticon-right-chevron"></i>
+                                        </a>
+                                    </li>
+                                                                    <li>
+                                        <a href="http://127.0.0.1:8000/services/farm-transport-services">Farm Transport Services
+                                            <i class="flaticon-right-chevron"></i>
+                                        </a>
+                                    </li>
+                                                                    <li>
+                                        <a href="http://127.0.0.1:8000/services/commercial-truck-transport">Commercial Truck Transport
+                                            <i class="flaticon-right-chevron"></i>
+                                        </a>
+                                    </li>
+                                                            </ul>
+                        </div>
+                        <div class="tj-sidebar-widget sidebar-documents">
+                            <h5 class="details_title">Documents</h5>
+                            <ul class="list-gap">
+                                <li>
+                                    <a href="#">
+                                        Our Company Details <br>
+                                        [450KB] <i class="fa-solid fa-cloud-arrow-down"></i></a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        Our Company Details <br>
+                                        [450KB] <i class="fa-solid fa-cloud-arrow-down"></i></a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="tj-appointment-box" data-bg-image="assets/images/service/service-15.jpg" style="background-image: url(&quot;assets/images/service/service-15.jpg&quot;);">
+                            <div class="tj-appointment-bg" data-bg-image="assets/images/banner/service_shape.png" style="background-image: url(&quot;assets/images/banner/service_shape.png&quot;);"></div>
+                            <div class="tj-appointment-body">
+                                <div class="appointment-percent text-center">
+                                    <span class="text-uppercase">100% <br>
+                                        Quality</span>
+                                </div>
+                                <div class="appointment-content text-center">
+                                    <h4 class="title">Make An Appointment</h4>
+                                    <a class="tel-link" href="tel:+51-(0)-888-455-369">1 (844) 474-4721</a>
+                                    <p>Perfectly simple &amp; easy to distinguish free hour when power.</p>
+                                    <div class="tj-theme-button">
+                                        <button class="tj-transparent-btn submit-btn2" type="submit" value="submit">
+                                            Get a Quote <i class="fa-light fa-arrow-right"></i>
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                
+    </div>
+    </div>
+    </section>
+<!--========== Privacy Policy end ==============-->
+
 @endsection
