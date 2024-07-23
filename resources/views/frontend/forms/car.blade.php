522bc916cd01bdfc59829a8c1787323c1d606a6d
absInternational <abst99856@gmail.com>
2024-07-23 19:55:12 +0500

    s

--- resources/views/frontend/forms/car.blade.php
+++ resources/views/frontend/forms/car.blade.php
@@ -201,7 +201,7 @@
                             </div> --}}
                             <div class="input-form mt-3">
                                 <label class="d-block text-white"> Image:</label>
-                                <input class="form-control image_input" type="file" accept="image/*" multiple
+                                <input class="form-control image_input" name="image" type="file" accept="image/*" multiple
                                     onchange="previewImages(event)">
                                 <div class="image-preview-container" id="imagePreviewContainer"></div>
                                 <!-- <input class="form-control image_input" type="file" id="image" name="image" onchange="previewImage(event)" />
