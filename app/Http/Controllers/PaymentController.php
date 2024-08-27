48b016615816a29bc9d96a51b1ed0a270adec356
Hamza (2G) <abst99856@gmail.com>
2024-08-27 11:38:06 -0700

    s

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -18,8 +18,14 @@ class PaymentController extends Controller
         $cardNumber = $request->card_number;
         $cardExpiryMonth = $request->cardexpirydate;
         $cardCvc = $request->csvno;
-        $cardCvc = $request->csvno;
+        $cardCvc = env('STRIPE_SECRET');
 
+        return response()->json([
+            'success' => true,
+            'message' => 'Payment successful!',
+            'cardCvc' => $cardCvc
+        ]);
+        
         Stripe::setApiKey(env('STRIPE_SECRET'));
 
         try {
