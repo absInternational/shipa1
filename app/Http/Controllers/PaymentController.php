bef7fd8eeac2cab5df91ba2c856718dc48c30b26
Hamza (2G) <abst99856@gmail.com>
2024-08-27 11:18:22 -0700

    s

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -19,8 +19,7 @@ class PaymentController extends Controller
         $cardExpiryMonth = $request->cardexpirydate;
         $cardCvc = $request->csvno;
 
-        $stripeSecret = 'sk_test_51Pqyz2Eb31S8Nw5LWQ7fyYqq3R67fSg4s2Cthm4ZNwu5qOlp21rxaBdSmfGxqTkUL0NkGotliu0w2AbzpE0vyYOv00GGs4IOG2';
-        Stripe::setApiKey($stripeSecret);
+        Stripe::setApiKey("sk_test_51Pqyz2Eb31S8Nw5LWQ7fyYqq3R67fSg4s2Cthm4ZNwu5qOlp21rxaBdSmfGxqTkUL0NkGotliu0w2AbzpE0vyYOv00GGs4IOG2");
 
         try {
             // Create a token
