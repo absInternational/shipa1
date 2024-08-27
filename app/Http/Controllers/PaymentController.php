0cf8799bc5e286f64c6baf34c3f288963c0ad684
Hamza (2G) <abst99856@gmail.com>
2024-08-27 09:47:21 -0700

    s

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -9,13 +9,13 @@ use Stripe\PaymentIntent;
 
 class PaymentController extends Controller
 {
-    public function createCharge(Request $request)
+    public function processPayment(Request $request)
     {
         return response()->json([
             'success' => true,
             'message' => 'Payment successful!'
         ]);
-        
+
         Log::info('Request data:', $request->all());
 
         $amount = $request->amount;
