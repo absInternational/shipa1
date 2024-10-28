638e8a66856ef9746615f87536ce02c55c287b71
Harris <acessonly11@gmail.com>
2024-10-28 08:39:58 -0700

    Update golf-cart-service.blade.php

--- resources/views/frontend/pages/services/golf-cart-service.blade.php
+++ resources/views/frontend/pages/services/golf-cart-service.blade.php
@@ -433,7 +433,7 @@
                 <div class="col-lg-4 col-md-12 order-sm-1 order-md-2 order-2 d-flex align-items-center justify-content-center sal-animate" data-sal="slide-left" data-sal-duration="800">
                     <div class="mt-4 rounded">
                         <div class="image-box">
-                            <img class="rounded" src="{{ asset('frontend/images/project/2a-image-for-golf-cart-transport.webp') }}" alt="Image">
+                            <img class="rounded" src="{{ asset('/public/frontend/images/project/2a-image-for-golf-cart-transport.webp') }}" alt="Image">
                         </div>
                     </div>
                 </div>
