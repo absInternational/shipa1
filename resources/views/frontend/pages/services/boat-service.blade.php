59f736f588ab8e7b0ff6f6b02e5e9371b86e3442
Harris <acessonly11@gmail.com>
2024-10-28 09:14:26 -0700

    Update boat-service.blade.php

--- resources/views/frontend/pages/services/boat-service.blade.php
+++ resources/views/frontend/pages/services/boat-service.blade.php
@@ -65,7 +65,7 @@
             z-index: 0;
         }
         .why-choose-us-slider {
-            background: url('../../img/1-background-image-for-boat-transport.webp');
+            background: url('/public/img/1-background-image-for-boat-transport.webp');
             padding: 60px 0;
             background-color: #f9f9f9;
             position: relative;
