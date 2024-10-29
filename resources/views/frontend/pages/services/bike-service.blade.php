f0be5493c2f84b0d489682a1a2b30cbc3c7c9b62
acessonly11 <acessonly11@gmail.com>
2024-10-29 05:21:09 -0700

    haris

--- resources/views/frontend/pages/services/bike-service.blade.php
+++ resources/views/frontend/pages/services/bike-service.blade.php
@@ -387,7 +387,6 @@
                 <div class="mt-4 rounded">
                     <div class="image-box">
                         <img class="rounded" src="{{ asset('public/frontend/images/project/2a-image-for-motorcycle-transport-_1_.webp') }}" alt="Image">
-                        {{-- <img class="rounded" src="{{ asset('public/frontend/images/project/2a-image-for-motorcycle-transport-_1_.webp') }}" alt="Image"> --}}
                     </div>
                 </div>
             </div>
