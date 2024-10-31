b69dd21979423cffa0a46d46f528c3f68e221efe
absinternational <abst99856@gmail.com>
2024-10-31 06:04:20 -0700

    s

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,24 +25,24 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // if ($this->app->environment('production')) {
-        //     URL::forceScheme('https');
-        // }
-
         if ($this->app->environment('production')) {
             URL::forceScheme('https');
-
-            $this->app['request']->server->set('HTTPS', true);
-            if (!str_starts_with(request()->getHost(), 'www')) {
-                $this->app['request']->server->set('HTTP_HOST', 'www.shipa1.com');
-            }
-
-            $this->app['router']->pushMiddlewareToGroup('web', function ($request, $next) {
-                if (!str_starts_with($request->getHost(), 'www')) {
-                    return Redirect::secure('www.' . $request->getHost() . $request->getRequestUri());
-                }
-                return $next($request);
-            });
         }
+
+        // if ($this->app->environment('production')) {
+        //     URL::forceScheme('https');
+
+        //     $this->app['request']->server->set('HTTPS', true);
+        //     if (!str_starts_with(request()->getHost(), 'www')) {
+        //         $this->app['request']->server->set('HTTP_HOST', 'www.shipa1.com');
+        //     }
+
+        //     $this->app['router']->pushMiddlewareToGroup('web', function ($request, $next) {
+        //         if (!str_starts_with($request->getHost(), 'www')) {
+        //             return Redirect::secure('www.' . $request->getHost() . $request->getRequestUri());
+        //         }
+        //         return $next($request);
+        //     });
+        // }
     }
 }
