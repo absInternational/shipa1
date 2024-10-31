3222587e03728a58d03a1952617e8f9a7bd4c119
absinternational <abst99856@gmail.com>
2024-10-31 06:02:20 -0700

    s

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Redirect;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,8 +25,24 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        // if ($this->app->environment('production')) {
+        //     URL::forceScheme('https');
+        // }
+
         if ($this->app->environment('production')) {
             URL::forceScheme('https');
+
+            $this->app['request']->server->set('HTTPS', true);
+            if (!str_starts_with(request()->getHost(), 'www')) {
+                $this->app['request']->server->set('HTTP_HOST', 'www.shipa1.com');
+            }
+
+            $this->app['router']->pushMiddlewareToGroup('web', function ($request, $next) {
+                if (!str_starts_with($request->getHost(), 'www')) {
+                    return Redirect::secure('www.' . $request->getHost() . $request->getRequestUri());
+                }
+                return $next($request);
+            });
         }
     }
 }
