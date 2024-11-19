27e1bf3d1f52f3e91db6b15e4905d2b0b60ad465
acessonly11 <acessonly11@gmail.com>
2024-11-19 11:25:13 -0800

    .

--- resources/views/auth/layouts/app.blade.php
+++ resources/views/auth/layouts/app.blade.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>SshiA1 - Register</title>
+    <title>ShipA1 - Register</title>
 
     <!-- Custom fonts for this template-->
     <link href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
