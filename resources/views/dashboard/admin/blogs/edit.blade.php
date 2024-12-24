f859a9fe08b71ab4e251f84f528c3ef505bb4279
Hamza <abst99856@gmail.com>
2024-12-24 20:51:18 +0500

    s

--- resources/views/dashboard/admin/blogs/edit.blade.php
+++ resources/views/dashboard/admin/blogs/edit.blade.php
@@ -63,6 +63,7 @@
 
                 <div class="form-group">
                     <label for="image">Image</label>
+                    
                     <input type="file" class="form-control-file" id="image" name="image">
                 </div>
 
