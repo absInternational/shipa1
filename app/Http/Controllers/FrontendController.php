e1654cb205ba2f8d067d2f579ae846555df1a7c9
Harris <acessonly11@gmail.com>
2025-01-15 23:41:03 +0500

    s

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -882,7 +882,6 @@ class FrontendController extends Controller
       
         try {
             $response = Http::post('https://washington.shawntransport.com/api/v2/submit_query', $post_array)->json();
-            dd($response);
             if (isset($response['status_code']) && $response['status_code'] == 201) {
                 return view('frontend.pages.thank-you');
             } else {
