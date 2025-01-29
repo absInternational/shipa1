ce22b605f0783c680836d21e08db8e9f93ddcf4b
Harris <acessonly11@gmail.com>
2025-01-29 23:47:36 +0500

    h

--- resources/views/dashboard/admin/layouts/app.blade.php
+++ resources/views/dashboard/admin/layouts/app.blade.php
@@ -155,9 +155,9 @@
             @include('dashboard.admin.includes.footer')
         </div>
     </div>
-    <a class="scroll-to-top rounded" href="#page-top">
-        <i class="fas fa-angle-up"></i>
-    </a>
+        <a class="scroll-to-top rounded" href="#page-top">
+            <i class="fas fa-angle-up"></i>
+        </a>
     <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
