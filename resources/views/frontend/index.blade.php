24b0f38da99be506ddd70841ece0a16f7fe8b630
Harris <acessonly11@gmail.com>
2025-02-06 23:47:19 +0500

    h

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -159,7 +159,7 @@ delivery. We focus on your satisfaction with reliable, affordable services.') @s
                 </div>
             </div> --}}
             <div class="swiper-wrapper">
-                <div class="swiper-slide d-flex align-items-center justify-content-center position-relative text-white" style="height: 750px; background-size: cover; background-position: center;" data-bg-image="{{ asset('frontend/images/slider/DSF.webp') }}">
+                <div class="swiper-slide d-flex position-relative text-white" style="height: 750px; background-size: cover; background-position: center;" data-bg-image="{{ asset('frontend/images/slider/DSF.webp') }}">
                     <div class="container text-center">
                         <div class="slider-content p-z-idex">
                             <h1 class="slider-title fw-bold">Trustworthy, Efficient, And Cost-effective Auto Transport Solutions</h1>
@@ -172,7 +172,7 @@ delivery. We focus on your satisfaction with reliable, affordable services.') @s
                     <div class="banner-shape"></div>
                 </div>
             
-                <div class="swiper-slide d-flex align-items-center justify-content-center position-relative text-white" style="height: 750px; background-size: cover; background-position: center;" data-bg-image="{{ asset('frontend/images/slider/home-slider-2.webp') }}">
+                <div class="swiper-slide d-flex position-relative text-white" style="height: 750px; background-size: cover; background-position: center;" data-bg-image="{{ asset('frontend/images/slider/home-slider-2.webp') }}">
                     <div class="container text-center">
                         <div class="slider-content p-z-idex">
                             <h4 class="slider-title fw-bold">Trustworthy, Efficient and Cost-Effective Transportation Services</h4>
@@ -185,7 +185,7 @@ delivery. We focus on your satisfaction with reliable, affordable services.') @s
                     <div class="banner-shape"></div>
                 </div>
             
-                <div class="swiper-slide d-flex align-items-center justify-content-center position-relative text-white" style="height: 750px; background-size: cover; background-position: center;" data-bg-image="{{ asset('frontend/images/slider/freight-banner-home.webp') }}">
+                <div class="swiper-slide d-flex position-relative text-white" style="height: 750px; background-size: cover; background-position: center;" data-bg-image="{{ asset('frontend/images/slider/freight-banner-home.webp') }}">
                     <div class="container text-center">
                         <div class="slider-content p-z-idex">
                             <h4 class="slider-title fw-bold">Trustworthy, Efficient and Cost-Effective Logistic Services</h4>
