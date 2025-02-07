e9cf5a39384f101878c6bbbed7fc756e890f0949
Harris <acessonly11@gmail.com>
2025-02-07 18:30:35 +0500

    h

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -12,7 +12,6 @@
 <meta name=twitter:title content="ShipA1 Auto Transport Quotes | Best Vehicle Shipping Service in USA">
 <meta name=twitter:description content="Get car shipping services in USA, scratchless vehicle transport service along with huge discount offers and FREE auto shipping quotes nationwide.">
 <meta name=twitter:image content=https://www.shipa1.com/img/car-tranportation.png>
-<link rel="preload" as="image" href="{{ asset('/frontend/images/logo/LOGO NEW-2.webp') }}" type="image/webp">
 <link rel=preload href="{{ asset('/frontend/images/logo/favicon.webp') }}" as=image type=image/png>
 <link rel=apple-touch-icon href="{{ asset('/frontend/images/logo/favicon.webp') }}">
 <link rel="shortcut icon" href="{{ asset('/frontend/images/logo/favicon.webp') }}" type=image/x-icon>
